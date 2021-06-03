868508d925091c7b13410623fc1309ad24064836
OlegMikus <mikus.oleg@gmail.com>
2021-06-03 19:53:41 +0300

    add rest framework settings

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -81,6 +81,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
